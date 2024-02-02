db24b95f182dd26f3869e6a6fbf5ed8939505bfb
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-02-02 17:54:37 -0300

    type the express response for education purposes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, HttpCode, Res } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  @HttpCode(200) /* 200 is the default. This decorator allows customization*/
+  /* @HttpCode(200) // 200 is the default. This decorator allows customization*/
   findAll(): object {
-    return { data: 'This action returns all cats'};
+    return { data: 'This action returns all cats' };
   }
 
+  /* 
+  The method below shows how to use custom express responses with @Res() response
+  parameter. The passthrough tells nest to use the express response object
+  without disabling the nestjs response object as the default for the remainder
+  of this route.
+  */
   @Get('breeds')
-  findAll2(@Res() response): string {
+  findAll2(@Res({ passthrough: true }) response: Response): Response {
     return response.status(200).send('Siamese cat');
   }
 }
